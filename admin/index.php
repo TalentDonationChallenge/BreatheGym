777096a23c421f8b24b33537cd435a74aaee6732
Jonggyu Shim <cham42666@naver.com>
2015-07-15 11:55:23 +0900

    repository reinitialized

---
+++ admin/index.php
@@ -0,0 +1,28 @@
+<?php 
+	require_once(__DIR__.'/../framework/framework.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php head('브리드 복싱 &amp; 크로스핏 - 관리자메뉴', 
+	array('/common/font-awesome/css/font-awesome.css',
+		'/common/css/style.css',
+		'/common/css/style-responsive.css'));?>
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<!--header start-->
+	<?php adminHeader(); ?>
+	<!--header end-->
+	<!--sidebar start-->
+	<?php adminSidebar("index"); ?>
+	<!--sidebar end-->
+	<script src="/common/js/jquery-1.11.1.min.js"></script>
+	<script src="/common/js/bootstrap.min.js"></script>
+	<script src="js/jquery.dcjqaccordion.2.7.js"></script>
+	<script src="js/common-scripts.js"></script>
+</body>
+</html>
