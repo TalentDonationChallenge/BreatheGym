e851b76efeda7e38527ff4e1d7441e4315282a4f
Jonggyu Shim <cham42666@naver.com>
2015-07-16 19:34:33 +0900

    회원기록관리 model 부분. 안되는게 맞습니다 여러분

--- framework/model/recordManage.php
+++ framework/model/recordManage.php
@@ -1,6 +1,48 @@
 <?php
-	require_once(__DIR__ . '/../framework/framework.php');
-	$pdo = Database::getInstance();
-	//가져와야 하는게, 그 날 한 운동종목이랑, 기록, 출석한 사람 명단
+	
+	/* 회원 운동기록 관리 */	
+	class AdminRecordManage {
+		//운동 기록 입력 함수
+		public static function insertRecord($barcodes, $exercises, $types, $record, $date) {
+			$pdo = Database::getInstance();
+			try {
+				for($i=0;$i<count($exercises);$i++){
+					for($j=0;$j<count($barcodes);$j++){
+						if($types[$i]===0){ //입력하는 운동이 횟수기록을 가질 경우
+							$stmt = $pdo->prepare('INSERT INTO exerciseRecord(barcode, name, type, timeRecord, countRecord, date)
+									VALUES(:barcode, :name, :type, :timeRecord, :countRecord, :date)');
+							$stmt->execute(array(
+									':barcode'=>$barcodes[$j],
+									':name'=>$exercises[$i],
+									':type'=>0,
+									':timeRecord'=>null,
+									':countRecord'=>$record,
+									':date'=>$date
+							));
+						} else if($types[$i]===1) { //입력하는 운동이 시간기록을 가질 경우
+							$stmt = $pdo->prepare('INSERT INTO exerciseRecord(barcode, name, type, timeRecord, countRecord, date)
+									VALUES(:barcode, :name, :type, :timeRecord :countRecord, :date)');
+							$stmt->execute(array(
+									':barcode'=>$barcodes[$j],
+									':name'=>$exercises[$i],
+									':type'=>1,
+									':timeRecord'=>$record,
+									':countRecord'=>null,
+									':date'=>$date
+							));
+						} else {
+							//여기론 오지마
+						}
+					}
+				}
+			} catch(Exception $e){
+
+			}
+		}
+		//현재 출석한 사람들 명단 받아오기
+		public static function showMembers(){ 
+
+		}
+	}
 	
 ?>
