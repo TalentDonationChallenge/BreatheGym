bc6f719d40af98eb7e0bb2b15bb6647aa99e8c08
jinuk <burningsweetpotato@gmail.com>
2015-07-16 13:31:48 +0900

    templete.php 코드 수정
    templete.php 코드 수정
    - 회원운동기록관리에 쓰일 member header, memberSidebar 코드 추

--- common/templete.php
+++ common/templete.php
@@ -95,7 +95,7 @@
 						<li><a href='#'>크로스핏 동작들</a></li>
 						</ul>
 					</li>
-					<li><a href='./exercise/'>운동관리</a></li>
+					<li><a href='#'>운동관리</a></li>
 					<li class='dropdown'>
 						<a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-expanded='false'>
 							커뮤니티<span class='caret'></span>
@@ -174,6 +174,65 @@
 						<span>기록관리</span>
 					</a>
 				</li>
+				</ul>
+			<!-- sidebar menu end-->
+			</div>
+		</aside>
+	<?php }
+	/* 관리자메뉴 header */
+	function memberHeader($active,$active2) { ?>
+		<header class='header black-bg'>
+			<div class='sidebar-toggle-box'>
+				<div class='fa fa-bars tooltips' data-placement='right' data-original-title='메뉴 보이기/끄기'></div>
+			</div>
+			<!--logo start-->
+			<a href=<?=$active2.'.php'?> class='logo'><b><?=$active?></b></a>
+			<!--logo end-->
+			<div class='top-menu'>
+				<ul class='nav pull-right top-menu'>
+				<li><a class='logout' href='/'>로그아웃</a></li>
+				</ul>
+			</div>
+		</header>
+	<?php } 
+	function memberSidebar($active) { ?>
+		<aside>
+			<div id='sidebar'  class='nav-collapse '>
+			<!-- sidebar menu start-->
+				<ul class='sidebar-menu' id='nav-accordion'>
+				<li class='mt'>
+					<a <?=$active==='index'?"class='active'":""?> href='index.php'>
+						<i class='fa fa-dashboard'></i>
+						<span>대쉬보드</span>
+					</a>
+				</li>
+
+				<li class='sub-menu'>
+					<a <?=$active==='member'?"class='active'":""?> href='/' >
+						<i class='fa fa-desktop'></i>
+						<span>HOME</span>
+					</a>
+				</li>
+
+				<li class='sub-menu'>
+					<a <?=$active==='analysis'?"class='active'":""?> href='boxing.php' >
+						<i class='fa fa-cogs'></i>
+						<span>복싱</span>
+					</a>
+				</li>
+				<li class='sub-menu'>
+					<a <?=$active==='exercise'?"class='active'":""?> href='crossfit.php' >
+						<i class='fa fa-book'></i>
+						<span>크로스핏</span>
+					</a>
+				</li>
+				<li class='sub-menu'>
+					<a <?=$active==='consulting'?"class='active'":""?> href='mypage.php' >
+						<i class='fa fa-tasks'></i>
+						<span>MyPage?</span>
+					</a>
+				</li>
+				
 				</ul>
 			<!-- sidebar menu end-->
 			</div>
