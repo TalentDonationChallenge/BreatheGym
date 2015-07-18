391e3e1e08d684cee2e5986c546c5bc567a8596e
lovelyhwado <lovelyhwado@gmail.com>
2015-07-18 23:15:56 +0900

    운동종류 json추가

--- database/dbsetup.php
+++ database/dbsetup.php
@@ -1,5 +1,19 @@
 <?php
 	require_once(__DIR__ . '/../framework/framework.php');
 	$pdo = Database::getInstance();
-   	$pdo->exec(file_get_contents("table.sql")); ?>
+	$pdo->exec(file_get_contents("table.sql")); 
+
+	$exercises = json_decode(file_get_contents("exerciseList.json"), true);
+
+	foreach ($exercises as $exercise) {
+		$stmt = $pdo->prepare("INSERT INTO exerciseList (name, type, time, count) 
+		VALUES (:name, :type, :time, :count)");
+		$stmt->execute(array(
+			":name"=>$exercise["name"],
+			":type"=>$exercise["type"],
+			":time"=>$exercise["time"],
+			":count"=>$exercise["count"]
+		));
+	}
+?>
 <a href="/index.php">gogo</a>
