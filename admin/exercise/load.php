c7a9cfe04fc55a83b84e7dfa0e8d86b910ca401a
lovelyhwado <lovelyhwado@gmail.com>
2015-07-18 22:50:51 +0900

    load.php 작성

--- admin/exercise/load.php
+++ admin/exercise/load.php
@@ -1,5 +1,27 @@
 <?php
 	require_once (__DIR__.'/../../framework/framework.php');
 
-	
+	$msg = array(
+		'status' => 'ok'
+	);
+
+	try {
+		if (!isset($_GET['requestType'])) throw new Exception('no request');
+		if ($_GET['requestType'] === 'exercise') {
+			$msg['exercises'] = AdminExerciseSchedule::loadExercise();
+		} else if ($_GET['requestType'] === 'schedule') {
+
+		} else {
+			new Exception('no request');
+		}
+
+	} catch (Exception $exception) {
+		$message['status'] = 'error';
+		$message['message'] = $exception->getMessage();
+		
+		header("HTTP/1.1 410 Gone");
+	}
+
+	header('Content-Type: application/json');
+	print(json_encode($msg));
 ?>
--- framework/model/admin/exercise.php
+++ framework/model/admin/exercise.php
@@ -7,27 +7,23 @@
 	class AdminExerciseSchedule {
 		public static function insertExcercise($name, $type,$value) { //운동 종류 추가할 때
 			$pdo = Database::getInstace();
-			try {
-				if ($type===0) { //일정시간동안 세트수
-					$stmt = $pdo->prepare('INSERT INTO exerciseList(name, type, time)
-						VALUES (:name, :type, :time) ');
-					$stmt->execute(array(
-						':name'=>$name,
-						':type'=>0,
-						':time'=>'00:'.$value["minute"].':'.$value["second"]
-					));
-				} else if($type===1) { //일정세트 하는데 걸린 시간
-					$stmt = $pdo->prepare('INSERT INTO exerciseList(name, type, count)
-						VALUES (:name, :type, :count) ');
-					$stmt->execute(array(
-						':name'=>$name,
-						':type'=>1,
-						':count'=>$count
-					));
-				} else {
-					
-				}
-			} catch (Exception $e) {
+			if ($type===0) { //일정시간동안 세트수
+				$stmt = $pdo->prepare('INSERT INTO exerciseList(name, type, time)
+					VALUES (:name, :type, :time) ');
+				$stmt->execute(array(
+					':name'=>$name,
+					':type'=>0,
+					':time'=>'00:'.$value["minute"].':'.$value["second"]
+				));
+			} else if($type===1) { //일정세트 하는데 걸린 시간
+				$stmt = $pdo->prepare('INSERT INTO exerciseList(name, type, count)
+					VALUES (:name, :type, :count) ');
+				$stmt->execute(array(
+					':name'=>$name,
+					':type'=>1,
+					':count'=>$count
+				));
+			} else {
 				
 			}
 		}
@@ -46,25 +42,20 @@
 		}
 
 		public static function loadExercises() {
-			try {
-				$pdo = Database::getInstance();
-				$stmt = $pdo->prepare('SELECT no, name
-					FROM exerciseList');
-				$stmt -> execute();
-				$rows = $stmt -> fetchAll();
+			$pdo = Database::getInstance();
+			$stmt = $pdo->prepare('SELECT no, name FROM exerciseList');
+			$stmt -> execute();
+			$rows = $stmt -> fetchAll();
 
-				$exercises = array();
+			$exercises = array();
 
-				foreach ($rows as $row) {
-					$exercises[] = array(
-						'no' => $row['no'],
-						'name' => $row['name']
-					);
-				}
-				return $exercises;
-			} catch (Exception $e) {
-				// 에러
+			foreach ($rows as $row) {
+				$exercises[] = array(
+					'no' => $row['no'],
+					'name' => $row['name']
+				);
 			}
+			return $exercises;
 		}
 		public static function loadSchedule($month) {
 			
