21884e60e336497480eade59744d3292ea5ae29f
jinuk <burningsweetpotato@gmail.com>
2015-07-19 20:00:43 +0900

    exercise 폴더 admin처럼 동일하게 구조화
    exercise폴더 admin처럼 동일하게 구조화 및 코드 일부 수정
    - common/templete.php member header, member sidebar 수정
    - exercise 폴더의 모든 php 코드 일부 수

--- exercise/crossfit/index.php
+++ exercise/crossfit/index.php
@@ -1,17 +1,16 @@
 <?php 
-	require_once(__DIR__.'/../framework/framework.php');
+	require_once(__DIR__.'/../../framework/framework.php');
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<?php head('크로스핏', 
-	array('./assets/font-awesome/css/font-awesome.css',
-		'./assets/css/style.css',
-		'./assets/css/style-responsive.css',
-		'./assets/css/table-responsive.css',
-		'./assets/css/to-do.css',
-		'./assets/css/zabuto_calendar.css',
-		'./assets/css/bootstrap.css'));?>
+	array('/exercise/css/font-awesome/css/font-awesome.css',
+        '/exercise/css/style.css',
+        '/exercise/css/style-responsive.css',
+        '/exercise/css/table-responsive.css',
+        '/exercise/css/to-do.css',
+        '/exercise/css/zabuto_calendar.css'));?>
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -20,26 +19,26 @@
 <body>
 
 	<!--header start-->
-	<?php memberHeader("크로스핏","crossfit"); ?>
+	<?php memberHeader(); ?>
 	<!--header end-->
 	<!--sidebar start-->
 	<?php memberSidebar("crossfit"); ?>
 	<!--sidebar end-->
-	 <section id = "main-content">
+		 <section id = "main-content">
                 <section class = wrapper>
                     <div class = "record">
                         <div role="tabpanel">
                         
                         <!-- Nav tabs -->
                             <ul class="nav nav-tabs" role="tablist">
-                                <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">A코스</a></li>
-                                <li role="presentation" class="ac1"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">B코스</a></li>
-                                <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">C코스</a></li>
+                                <li role="presentation" class="active"><a href="#courseA" aria-controls="courseA" role="tab" data-toggle="tab">A코스</a></li>
+                                <li role="presentation" ><a href="#courseB" aria-controls="courseB" role="tab" data-toggle="tab">B코스</a></li>
+                                <li role="presentation" ><a href="#courseC" aria-controls="courseC" role="tab" data-toggle="tab">C코스</a></li>
                                 
                             </ul>
                             <!-- Tab panes -->
-                            <div class="tab-content">
-                                <div role="tabpanel" class="tab-pane active" id="home">
+                            <div class="tab-content">   
+                            	 <div role="tabpanel" class="tab-pane active" id="courseA">
                                 		<div class="memberrecordtable">
 					                        <div class="content-panel">
 					                            
@@ -75,8 +74,7 @@
 					                    </div><!-- /col-md-12 -->
 
                                 </div>
-                            <div role="tabpanel" class="tab-pane" id="profile">
-                            	  <div role="tabpanel" class="tab-pane active" id="home">
+                                <div role="tabpanel" class="tab-pane" id="courseB">
                                 		<div class="memberrecordtable">
 					                        <div class="content-panel">
 					                            
@@ -112,10 +110,7 @@
 					                    </div><!-- /col-md-12 -->
 
                                 </div>
-
-                            </div>
-                            <div role="tabpanel" class="tab-pane" id="messages">
-                            	  <div role="tabpanel" class="tab-pane active" id="home">
+                                 <div role="tabpanel" class="tab-pane" id="courseC">
                                 		<div class="memberrecordtable">
 					                        <div class="content-panel">
 					                            
@@ -151,14 +146,13 @@
 					                    </div><!-- /col-md-12 -->
 
                                 </div>
-                            </div>
-                                
-                            </div>
-                            
 
-                        </div>
-                    </div>
-                    <div class="myrecordtable">
+                                       
+                 			</div>
+                 
+                    </div><!-- /col-md-12 -->
+                 </div>
+                  <div class="myrecordtable">
                         <div class="content-panel">
                             <h4><i class="fa fa-angle-right"></i> 회원 개인 기록</h4>
                             <hr>
@@ -228,28 +222,21 @@
                     </div>
                 </section>
             </section>
-            <footer class="site-footer">
-                <div class="text-center">
-                    2015 - 크레센도
-                    <a href="crossfit.php#" class="go-top">
-                        <i class="fa fa-angle-up"></i>
-                    </a>
-                </div>
-            </footer>
+           
 		</section>
-	    <script src="assets/js/jquery.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
-        <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
-        <script src="assets/js/jquery.scrollTo.min.js"></script>
-        <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
+	    <script src="/exercise/js/jquery.js"></script>
+        <script src="/exercise/js/bootstrap.min.js"></script>
+        <script class="include" type="text/javascript" src="/exercise/js/jquery.dcjqaccordion.2.7.js"></script>
+        <script src="/exercise/js/jquery.scrollTo.min.js"></script>
+        <script src="/exercise/js/jquery.nicescroll.js" type="text/javascript"></script>
         
         
         <!--common script for all pages-->
         <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
         <script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
-        <script src="assets/js/common-scripts.js"></script>
+        <script src="/exercise/js/common-scripts.js"></script>
         
         <!--script for this page-->
-        <script src="assets/js/morris-conf.js"></script>
+        <script src="/exercise/js/morris-conf.js"></script>
 </body>
 </html>
