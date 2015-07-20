2e48871075131fa01fffba3caad348ec121d5a1a
jinuk <burningsweetpotato@gmail.com>
2015-07-21 01:22:14 +0900

    회원운동기록관리 메인의 쓸모없는 css,js 삭제
    exercise/index.php 코드에서 쓰지 않던 css,js 코드 삭제

--- exercise/index.php
+++ exercise/index.php
@@ -5,12 +5,9 @@
 <html>
 <head>
 	<?php head('브리드 복싱 &amp; 크로스핏-회원운동기록관리', 
-	 array('/exercise/css/font-awesome/css/font-awesome.css',
-        '/exercise/css/style.css',
-        '/exercise/css/style-responsive.css',
-        '/exercise/css/table-responsive.css',
-        '/exercise/css/to-do.css',
-        '/exercise/css/zabuto_calendar.css'));?>
+	array('/exercise/css/font-awesome/css/font-awesome.css',
+        '/exercise/css/style.css'
+        ));?>
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -108,19 +105,8 @@
 			<!--maincontent end-->
             
 		</section>
-	    <script src="/exercise/js/jquery.js"></script>
+	     <script src="/exercise/js/jquery.js"></script>
         <script src="/exercise/js/bootstrap.min.js"></script>
-        <script class="include" type="text/javascript" src="/exercise/js/jquery.dcjqaccordion.2.7.js"></script>
-        <script src="/exercise/js/jquery.scrollTo.min.js"></script>
-        <script src="/exercise/js/jquery.nicescroll.js" type="text/javascript"></script>
-        
-        
-        <!--common script for all pages-->
-        <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-        <script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
         <script src="/exercise/js/common-scripts.js"></script>
-        
-        <!--script for this page-->
-        <script src="/exercise/js/morris-conf.js"></script>
 </body>
 </html>
