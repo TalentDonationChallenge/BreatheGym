d447158985cbf0af0bd669d4746dbde07b5fd2e1
Jonggyu Shim <cham42666@naver.com>
2015-07-30 21:20:22 +0900

    복싱 진도 정보 가져오는 함수 작성

---
+++ framework/model/exercise/boxing.php
@@ -0,0 +1,27 @@
+<?php
+	/* 회원 복싱 진도 확인*/
+	class MemberBoxingManage{
+		public static function getBoxingStep($memberName){
+			$pdo = Database::getInstance();
+			$stmt=$pdo->prepare('SELECT no, name, youtubeSrc, description, summary, photo from boxingLevel join boxingList on boxingLevel.no = boxingList.no where barcode=:member');
+			$stmt->execute(array(':member'=>$memberName));
+			$result=$stmt->fetchAll();
+			$boxingInfo = array();
+			foreach($result as $row){
+				$boxingInfo[] = array(
+					'no' => $row['no'],
+					'name' => $row['name'],
+					'youtubeSrc' => $row['youtubeSrc'],
+					'description' => $row['description'],
+					'summary' => $row['summary'],
+					'photo' => $row['photo']
+				);
+			}
+
+			return $boxingInfo;
+		}
+
+		
+
+	}
+?>
