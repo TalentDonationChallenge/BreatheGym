5d4b5effff1b6f6c52e5c970b6685d4845e3aafe
lovelyhwado <lovelyhwado@gmail.com>
2015-08-12 15:56:44 +0900

    기록관리 세부수정 남은건 낼할께여 데헷(웃음)

--- framework/model/admin/record.php
+++ framework/model/admin/record.php
@@ -2,7 +2,7 @@
 
 	/* 회원 운동기록 관리 */
 	class AdminRecordManage {
-		// //운동 기록 입력 함수
+		//운동 기록 입력 함수
 		public static function insertRecord($members, $exercise) {
 			$pdo = Database::getInstance();
 			foreach ($members as $member) {
@@ -23,6 +23,23 @@
 						':timeRecord'=>$time,
 						':countRecord'=>"0"
 					));
+				} else {
+					throw new Exeception('wrong variable');
+				}
+			}
+		}
+		//운동 기록 수정 함수
+		public static function editRecord($members, $exercise) {
+			$pdo = Database::getInstance();
+			foreach ($members as $members ) {
+				if ($exercise['exerciseType']=== '0') {
+					$stmt = $pdo->prepare('UPDATE exerciseRecord
+						SET countRecord=:countRecord
+						WHERE barcode=:barcode AND exerciseNo=:exerciseNo');
+				} else if ($exercise['exerciseType'] === '1') {
+					$stmt = $pdo->prepare('UPDATE exerciseRecord
+						SET timeRecord=:timeRecord
+						WHERE barcode=:barcode AND exerciseNo=:exerciseNo');
 				} else {
 					throw new Exeception('wrong variable');
 				}
@@ -56,14 +73,14 @@
 			}
 			return $members;
 		}
-
+		//운동기록이 입력된 사람들의 명단과 기록 받아오기
 		public static function getFilledMemebers($date, $exercise, $branch) {
 			$tomorrow = date('Y-m-d',strtotime("+1day ".$date));
 			$pdo=Database::getInstance();
 			$stmt=$pdo->prepare('SELECT name, barcode, date, timeRecord, countRecord
 				FROM gymMember NATURAL JOIN exerciseRecord NATURAL JOIN attendance
 				WHERE date>=:today AND date<:tomorrow
-				AND exerciseNo = :exercise AND branch = :branch');
+				AND exerciseNo = :exercise AND branch = :branch ORDER BY date DESC');
 			$stmt->execute(array(
 				':today'=>$date,
 				':tomorrow'=>$tomorrow,
