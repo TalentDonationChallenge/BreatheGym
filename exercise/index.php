47ebb210b0af558c66a51220bdc2a922e6031310
lovelyhwado <lovelyhwado@gmail.com>
2015-08-25 13:12:59 +0900

    dashboard

--- exercise/index.php
+++ exercise/index.php
@@ -8,7 +8,8 @@
 	<?php head('브리드 복싱 &amp; 크로스핏-회원운동기록관리',
 	array('/common/font-awesome/css/font-awesome.css',
           '/common/css/style.css',
-          '/common/css/style-responsive.css'
+          '/common/css/style-responsive.css',
+		  'css/dashboard.css'
         ));?>
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -38,9 +39,9 @@
 	<section id = "main-content">
 		<section class = "wrapper">
 			<div class = "row">
-				<div class = "col-lg-11 main-chart" >
-					<div class = "row mtbox">
-						<div class = "col-md-3 col-sm-5 mb">
+				<div class = "col-lg-12 mt">
+					<div class = "row">
+						<div class = "col-md-3 col-sm-6 mb">
 							<div class="white-panel pn">
 								<div class = "white-header">
 									<h5>크로스핏 순위</h5>
@@ -48,21 +49,21 @@
 								<h3>상위 %</h3>
 							</div>
 						</div>
-						<div class = "col-md-3 col-sm-5 mb">
+						<div class = "col-md-3 col-sm-6 mb">
 							<div class="white-panel pn">
 								<div class = "white-header">
 									<h5>복싱 진도</h5>
 								</div>
 							</div>
 						</div>
-						<div class = "col-md-3 col-sm-5 mb">
+						<div class = "col-md-3 col-sm-6 mb">
 							<div class="white-panel pn">
 								<div class = "white-header">
 									<h5>연속출석일</h5>
 								</div>
 							</div>
 						</div>
-						<div class = "col-md-3 col-sm-5 mb">
+						<div class = "col-md-3 col-sm-6 mb">
 							<div class="white-panel pn">
 								<div class = "white-header">
 									<h5>등록일</h5>
@@ -72,41 +73,33 @@
 					</div>
 				</div>
 			<!--복싱진도가 보일 부분 start-->
-			<div class = "menucontainer">
-				<div class = "col-md-6 col-sm-6 box0">
-					<a href="/exercise/boxing/index.php">
-					<div>
-						<h4><i class = "fa fa-angle-right"></i>오늘의 복싱 진도</h4>
-						<div class = "boxing">
-						<img src = "/exercise/img/boxingimg.jpg" width="100%" height="260px">
-						</div>
-						<div class ="progress">
-						<div class = "progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax = "100" style="width : 20%">
-						<span>잽운동</span>
-						</div>
+			<div class = "col-lg-12">
+				<div class="row">
+					<div class = "col-md-6 col-sm-6 mb">
+						<div class="darkblue-panel pn">
+							<div class="darkblue-header">
+								<h5>오늘의 복싱 진도</h5>
+							</div>
+							<img src = "/exercise/img/boxing.jpg">
+							<div class ="progress">
+								<div class = "progress-bar progress-bar-success" role="progressbar"
+								aria-valuenow="40" aria-valuemin="0" aria-valuemax = "100" style="width : 20%">
+								<span>잽운동</span>
+								</div>
+							</div>
 						</div>
 					</div>
-					</a>
-				</div>
-				<!--복싱진도가 보일 부분 end -->
-				<!--크로스핏 부분 start -->
-				<div class = "col-md-6 col-sm-6 box0">
-				<a href="/exercise/crossfit/index.php">
-				<div>
-				<h4><i class = "fa fa-angle-right"></i>오늘의 크로스핏</h4>
-				<div class = "boxing">
-				<img src = "/exercise/img/crossfit.jpg" width="100%" height="260px">
-				</div>
-
-				<div id = "mrecord">
-				<h4>오늘 회원님은 상위 <!--<?php //printf($myrecordPercentage)?>-->%의 기록을 달성하였습니다.</h4>
-				</div>
-
-				</div>
-
-
-				</a>
-
+					<!--복싱진도가 보일 부분 end -->
+					<!--크로스핏 부분 start -->
+					<div class = "col-md-6 col-sm-6 mb">
+						<div class="darkblue-panel pn">
+							<div class="darkblue-header">
+								<h5>오늘의 크로스핏</h5>
+							</div>
+							<img src = "/exercise/img/crossfit.jpg">
+							<h5>오늘 회원님은 상위 <!--<?php //printf($myrecordPercentage)?>-->%의 기록을 달성하였습니다.</h5>
+						</div>
+					</div>
 				</div>
 			</div>
 			<!--크로스핏 부분 end -->
