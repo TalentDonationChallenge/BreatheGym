71e66d0e5e70807938e513704863f1f784ee9beb
lovelyhwado <lovelyhwado@gmail.com>
2015-09-02 21:58:28 +0900

    상담 내용작성창

--- admin/consulting/view.php
+++ admin/consulting/view.php
@@ -7,7 +7,7 @@
 	<?php head('브리드 복싱 &amp; 크로스핏 - 관리자메뉴',
 	array('/common/font-awesome/css/font-awesome.css',
 		'/common/css/style.css','/common/css/style-responsive.css',
-		'/common/css/table-responsive.css'));?>
+		'/common/css/table-responsive.css','/admin/css/consulting.css'));?>
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -20,12 +20,58 @@
 	<!--sidebar start-->
 	<?php adminSidebar("consulting"); ?>
 	<!--sidebar end-->
+	<!-- 글번호를 GET으로 받아와서 고고싱하기로 하겠습니다 -->
 	<section id="main-content">
 		<section class="wrapper">
 			<h3><i class="fa fa-angle-right"></i> 상담</h3>
 			<div class="col-lg-12 mt">
-				<div class="panel panel-default">
-
+				<div class="panel panel-default mb">
+					<div class="panel-heading">
+						<span class="pull-right hidden-phone time">2015/09/03 10:21:58</span>
+						<span class="pull-right author">아이유</span>
+						<h2 class="panel-title">티키타 리듬에 맞춰 스핀 기타 리프의 테마는 스팅의 쉡오마핱</h2>
+					</div>
+					<div class="panel-body">
+						<p>
+							투명 드래곤 텍본<br/>
+							by 라크<br/><br/>
+							"크아아아아"<br/><br/>
+							드래곤중에서도 최강의 투명드래곤이 울부짓었다<br/>
+							투명드래곤은 졸라짱쎄서 드래곤중에서 최강이엇다<br/>
+							신이나 마족도 이겼따 다덤벼도 이겼따 투명드래곤은<br/>
+							새상에서 하나였다 어쨌든 걔가 울부짓었다<br/><br/>
+							"으악 제기랄 도망가자"<br/><br/>
+							발록들이 도망갔다 투명드래곤이 짱이었따<br/>
+							그래서 발록들은 도망간 것이다.<br/><br/>
+							투명드래곤은 심심햇다<br/>
+							그래서 신을죽이기로 햇다<br/>
+							그래서 신들은 비상이 걸렸따<br/><br/>
+							"씨 + 발 투명드래곤이 쳐들어온대"<br/><br/>
+							"그래 싸우자"<br/><br/>
+							하지만 투명드래곤은 투명드래곤이라서 투명했따<br/>
+							그래서 안보여서 신들은결국 다 죽고말았따<br/>
+							투명드래곤은 이새계가심심해서 다른새계로<br/>
+							가기로하였따ㅣ<br/><br/>
+							위이ㅣ이이이이이잉<br/>
+							투명드레곤은 차원이동을햇다<br/>
+							그러자 현실새계가 나왓다<br/><br/>
+							"오 조은데 심심한데 다주겨야지"<br/><br/>
+							투명드래곤이 브레스를했다 그러자 아니 브레스도<br/>
+							안하고그냥 손에서빔을 쐈다<br/>
+							그거 한방에미국이 다날라갔따<br/>
+							졸라짱쎈 투명드래곤이었다<br/>
+							사람들은투명드래곤이 투명해서 누가한지도 몰랐다<br/>
+							투명드래곤은 또 심심해져서결국......................<br/><br/>
+						</p>
+					</div>
+				</div>
+				<h4><i class="fa fa-angle-right"></i> 답변</h4>
+				<textarea class="form-control answer"></textarea>
+				<div class="buttons mt">
+					<button class="btn btn-default" name="button">이전글</button>
+					<button class="btn btn-default" name="button">다음글</button>
+					<button class="btn btn-default" name="button">목록</button>
+					<button class="btn btn-primary pull-right" name="button">등록</button>
 				</div>
 			</div>
 		</section>
