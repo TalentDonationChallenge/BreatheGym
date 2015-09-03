7e2955f897ab15b01756dba8edeca12ee5d1bce6
Jonggyu Shim <cham42666@naver.com>
2015-09-03 10:35:13 +0900

    facebook login

--- common/js/facebook.js
+++ common/js/facebook.js
@@ -1,3 +1,27 @@
+		window.fbAsyncInit = function(){
+			FB.init({
+				appId : '114295028916691',
+				cookie : true,
+				channelURL : 'localhost/index.php',
+				xfbml : true,
+				version : 'v2.4'
+			});
+			
+			FB.getLoginStatus(function(response) {
+    			statusChangeCallback(response);
+  			});
+		};
+
+
+		(function(d, s, id) {
+  			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+	 		js = d.createElement(s); js.id = id;
+	 		js.src = "//connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v2.4&appId=114295028916691";
+	 		fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+
+
 		function statusChangeCallback(response) {
 			alert("statusChangeCallback");
 		    console.log('statusChangeCallback');
@@ -7,8 +31,8 @@
 		    // Full docs on the response object can be found in the documentation
 		    // for FB.getLoginStatus().
 		    if (response.status === 'connected') {
-		      // Logged into your app and Facebook.
-		      testAPI();
+		        // Logged into your app and Facebook.
+		        testAPI();
 		    } else if (response.status === 'not_authorized') {
 		     	// The person is logged into Facebook, but not your app.
 		      	logoutAPI();
@@ -19,33 +43,12 @@
 		    }
 		  }
 
-		window.fbAsyncInit = function(){
-			FB.init({
-				appId : '114295028916691',
-
-				cookie : true,
-				xfbml : true,
-				version : 'v2.4'
-			});
-			
-			FB.getLoginStatus(function(response) {
-    			statusChangeCallback(response);
-  			});
-		};
-
-		(function(d, s, id) {
-  			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-	 		js = d.createElement(s); js.id = id;
-	 		js.src = "//connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v2.4&appId=114295028916691";
-	 		fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
 
 		function login(){
 			console.log("login function");
 			FB.login(function(response){
 					statusChangeCallback(response);
-				}, {scope:'public_profile, email'})
+			}, {scope:'public_profile, email'})
 		}
 
 		function logout(){
@@ -62,7 +65,6 @@
 			});
 		}
 
-
 		// Function implemented after logging in
 		function testAPI(){
 				alert("testAPI function");
@@ -73,8 +75,8 @@
 					logout();
 				});
 				FB.api('/me', function(response) {
-					console.log('Successful login for : ' + response.name);
-					'Thanks for loggin in, '+ response.name + '!';
+					console.log('Successful login for : ' + response.name +
+					'Thanks for loggin in, '+ response.name + '!');
 				});
 			}
 
