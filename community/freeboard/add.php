19e4595dbfd5b983b9f230b2a535effcb1e2bf9f
lovelyhwado <lovelyhwado@gmail.com>
2015-09-19 15:53:53 +0900

    자유게시판 기능

--- community/freeboard/add.php
+++ community/freeboard/add.php
@@ -2,6 +2,8 @@
     $board = new ImageBoard('freeboard');
     if ($_POST['requestType']==='posting') {
         $board->insertPost($_SESSION['email'], $_POST['title'], $_POST['content']);
+    } else if($_POST['requestType']==='comment'){
+        $board->submitComments($_SESSION['email'], $_POST['content'], $_POST['postNumber']);
     }
 
 ?>
--- community/js/freeboard.js
+++ community/js/freeboard.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
         location.href="write.php";
     });
     $('nav>button.submit').click(function () {
+        //여기 예외처리해야함
         $.ajax({
             url: 'add.php',
             method:'post',
@@ -13,6 +14,19 @@ $(document).ready(function () {
             }
         }).done(function () {
             location.href="index.php";
+        });
+    });
+    $('.box-reply .btn-option').click(function () {
+        $.ajax({
+            url: 'add.php',
+            method: 'post',
+            data : {
+                requestType: 'comment',
+                content : $('.write-comm textarea').val(),
+                postNumber : $('.panel-heading').attr('no')
+            }
+        }).done(function () {
+            location.reload();
         });
     });
 });
