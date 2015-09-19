1d8e443f06dbbe75b91245734c4ab6aa6c815214
Burningsweetpotato <87genesis@hanmail.net>
2015-09-19 21:34:15 +0900

    signFromWeb.php
    쓸모없는 코드 삭제용...코드 또 수정...

--- signFromWeb.php
+++ signFromWeb.php
@@ -2,9 +2,7 @@
 
     //홈페이지에서 가입양식을 통해 회원가입을 할때 사용하는 php 파일입니다.
 	//클라이언트에서 양식 폼에 맞게 작성한 데이터를 받아 db에 등록된 회원인지의 여부를 살펴보고 그 결과에 따라 다른 메세지를 클리이언트로 보내주는것이 목적입니다.
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-echo "start";
+
 	require_once(__DIR__.'/framework/framework.php');
 	
 	if(isset($_POST['userEmail']) && isset($_POST['userPassword']) && isset($_POST['userPhone']) && isset($_POST['sexRadio']) &&isset($_POST['userName']) && isset($_POST['userNickName'])){
