2caee51117b7eaa08ed84fdf3012568a757286b6
Na-Ick-Chae <ni37fsl@gmail.com>
2015-09-19 16:21:47 +0900

    크로스핏 UI 재수정
    row를 추가해서 불필요한 마진을 재수정함

--- exerciseInfo/crossfitLibrary/index.php
+++ exerciseInfo/crossfitLibrary/index.php
@@ -29,25 +29,125 @@ require_once(__DIR__.'/../../framework/framework.php');
 	<section id="main-content">
 		<section class="wrapper">
 			<div class="container">
-				<h3><i class="fa fa-angle-right ml"></i> 크로스핏 자료실</h3>
-
-				<div class="col-sm-3 mt">
-					<div class="panel panel-default cross-panel">
-						<div class="panel-body">
-							<img src="/introduction/images/ali.jpg" alt="Boxer Ali">
-						</div>
-						<div class="panel-footer">
-							<span class="inline-to-block">제목</span>
-							<span>조회 89</span>
-							<span> &#124; 15.07.31</span>
-							<span class="inline-to-block">글쓴이</span>
+				<h3><i class="fa fa-angle-right"></i> 크로스핏 자료실</h3>
+				<div class="row">
+					<div class="col-sm-3 mt">
+						<div class="panel panel-default cross-panel">
+							<div class="panel-body">
+								<img src="/introduction/images/ali.jpg" alt="Boxer Ali">
+							</div>
+							<div class="panel-footer">
+								<span class="inline-to-block">제목</span>
+								<span>조회 89</span>
+								<span> &#124; 15.07.31</span>
+								<span class="inline-to-block">글쓴이</span>
+							</div>
 						</div>
 					</div>
+
+					<div class="col-sm-3 mt">
+						<div class="panel panel-default cross-panel">
+							<div class="panel-body">
+								<img src="/introduction/images/ali.jpg" alt="Boxer Ali">
+							</div>
+							<div class="panel-footer">
+								<span class="inline-to-block">제목</span>
+								<span>조회 89</span>
+								<span> &#124; 15.07.31</span>
+								<span class="inline-to-block">글쓴이</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-3 mt">
+						<div class="panel panel-default cross-panel">
+							<div class="panel-body">
+								<img src="/introduction/images/ali.jpg" alt="Boxer Ali">
+							</div>
+							<div class="panel-footer">
+								<span class="inline-to-block">제목</span>
+								<span>조회 89</span>
+								<span> &#124; 15.07.31</span>
+								<span class="inline-to-block">글쓴이</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-3 mt">
+						<div class="panel panel-default cross-panel">
+							<div class="panel-body">
+								<img src="/introduction/images/ali.jpg" alt="Boxer Ali">
+							</div>
+							<div class="panel-footer">
+								<span class="inline-to-block">제목</span>
+								<span>조회 89</span>
+								<span> &#124; 15.07.31</span>
+								<span class="inline-to-block">글쓴이</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-3 mt">
+						<div class="panel panel-default cross-panel">
+							<div class="panel-body">
+								<img src="/introduction/images/ali.jpg" alt="Boxer Ali">
+							</div>
+							<div class="panel-footer">
+								<span class="inline-to-block">제목</span>
+								<span>조회 89</span>
+								<span> &#124; 15.07.31</span>
+								<span class="inline-to-block">글쓴이</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-3 mt">
+						<div class="panel panel-default cross-panel">
+							<div class="panel-body">
+								<img src="/introduction/images/ali.jpg" alt="Boxer Ali">
+							</div>
+							<div class="panel-footer">
+								<span class="inline-to-block">제목</span>
+								<span>조회 89</span>
+								<span> &#124; 15.07.31</span>
+								<span class="inline-to-block">글쓴이</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-3 mt">
+						<div class="panel panel-default cross-panel">
+							<div class="panel-body">
+								<img src="/introduction/images/ali.jpg" alt="Boxer Ali">
+							</div>
+							<div class="panel-footer">
+								<span class="inline-to-block">제목</span>
+								<span>조회 89</span>
+								<span> &#124; 15.07.31</span>
+								<span class="inline-to-block">글쓴이</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-3 mt">
+						<div class="panel panel-default cross-panel">
+							<div class="panel-body">
+								<img src="/introduction/images/ali.jpg" alt="Boxer Ali">
+							</div>
+							<div class="panel-footer">
+								<span class="inline-to-block">제목</span>
+								<span>조회 89</span>
+								<span> &#124; 15.07.31</span>
+								<span class="inline-to-block">글쓴이</span>
+							</div>
+						</div>
+					</div>
+
 				</div>
 
-					<nav>
-						<ul class="pagination">
-							<?php
+				<nav>
+					<ul class="pagination">
+						<?php
 					$allPages = $crossfitlibrary->pageCount(); // 다음부터 수정(a.k.a. 복붙)할때 이부분에 게시판 이름을 수정하면 된다
 					$pagingStart = $page%5==0?$page-4:$page-($page%5)+1; ?>
 					<?=$page<=5?'':
@@ -71,7 +171,7 @@ require_once(__DIR__.'/../../framework/framework.php');
 				</li>':''?>
 			</ul>
 
-			<button type="button" class="btn pull-right btn-primary btn-write mr">
+			<button type="button" class="btn pull-right btn-primary btn-write">
 				글쓰기
 			</button>
 		</nav>
