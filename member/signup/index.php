7bcd52a2cbc5279e8b456ab8a5f77d8a2dfcc9ea
Na-Ick-Chae <ni37fsl@gmail.com>
2015-10-03 14:38:22 +0900

    잡일

--- member/signup/index.php
+++ member/signup/index.php
@@ -4,7 +4,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<?php head('브리드 복싱 &amp; 크로스핏', array('/common/css/navigation.css')); ?>
+	<?php head('브리드 복싱 &amp; 크로스핏', array('/common/css/navigation.css','/member/css/member.css')); ?>
 	<!--[if lt IE 9]>
 	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
@@ -32,7 +32,6 @@
 				<label for="email" class='col-xs-2'>이메일</label>
 				<div class='col-xs-9'>
 					<input type='email' class="form-control" id='email' placeholder='Input Email' name = "userEmail">
-					<span class='glyphicon glyphicon-ok form-control-feedback' aria-hidden='true'></span>
 					<span class='glyphicon glyphicon-danger-sign form-control-feedback' aria-hidden='true'></span>
 					<div class='alert alert-danger' id='emailAlert' role='alert' hidden='true'>올바른 이메일을 입력해주세요! 예시)email@breathegym.gym</div>
 				</div>
@@ -45,7 +44,6 @@
 				<label for="password" class='col-xs-2'>비밀번호</label>
 				<div class='col-xs-10'>
 					<input type='password' class='form-control' id='password' placeholder='Input Password' name = "userPassword">
-					<span class='glyphicon glyphicon-ok form-control-feedback' aria-hidden='true'></span>
 					<span class='glyphicon glyphicon-danger-sign form-control-feedback' aria-hidden='true'></span>
 					<div class='alert alert-danger' id='passwordAlert' role='alert' hidden='true'>영문자 대소문자, 숫자가 포함되어야 합니다.</div>
 				</div>
@@ -55,33 +53,35 @@
 				<label for='password' class='col-xs-2'>비밀번호<br/>확인</label>
 				<div class='col-xs-10'>
 					<input type='password' class='form-control' id='passwordConfirm' placeholder='Input Password again'>
-					<span class='glyphicon glyphicon-ok form-control-feedback' aria-hidden='true'></span>
 					<span class='glyphicon glyphicon-danger-sign form-control-feedback' aria-hidden='true'></span>
 					<div class='alert alert-danger' id='passwordConfirmAlert' role='alert' hidden='true'>같은 비밀번호를 입력하세요.</div>
 				</div>
 			</div>
 			<br/>
+
 			<div class='row'>
-				<div class='form-group col-xs-6'>
-					<label for='name' class='col-xs-4'>이름</label>
-					<div class='col-xs-8'>
-						<input type='text' class='form-control' id='name' placeholder='Input Name' name = "userName">
+					<div class='form-group col-xs-6'>
+						<label for='name' class='col-xs-4 ml'>이름</label>
+						<div class='col-xs-6'>
+							<input type='text' class='form-control' id='name' placeholder='Input Name' name = "userName">	
+						</div>
+					</div>
+
+					<div class='form-group col-xs-6'>
+						<label for='sex' class='col-xs-2'>성별</label>
+						<div class='radio col-xs-8 pr' id='sex'>
+							<label class='radio-inline'>
+								<input type='radio' name='sexRadio' id='optionSex1' value='male' name = "userSex">
+								남자
+							</label>
+							<label class='radio-inline'>
+								<input type='radio' name='sexRadio' id='optionSex2' value='female' name = "userSex">
+								여자
+							</label>
+						</div>
 					</div>
 				</div>
-				<div class='form-group col-xs-6'>
-					<label for='sex' class='col-xs-4'>성별</label>
-					<div class='radio col-xs-8' id='sex'>
-						<label class='radio-inline'>
-							<input type='radio' name='sexRadio' id='optionSex1' value='male' name = "userSex">
-							남자
-						</label>
-						<label class='radio-inline'>
-							<input type='radio' name='sexRadio' id='optionSex2' value='female' name = "userSex">
-							여자
-						</label>
-					</div>
-				</div>
-			</div>
+
 			<br/>
 			<div class='form-group'>
 				<label for='닉네임' class='col-xs-2'>닉네임</label>
