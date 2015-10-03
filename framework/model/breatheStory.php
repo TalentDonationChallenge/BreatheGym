d339065ab71db926212ae1badeb9df4b65674a71
Jonggyu Shim <cham42666@naver.com>
2015-10-03 15:02:25 +0900

    imageBoard 상속

--- framework/model/breatheStory.php
+++ framework/model/breatheStory.php
@@ -1,9 +1,9 @@
 <?php
-require_once(__DIR__.'/board.php');
+require_once(__DIR__.'/imageBoard.php');
     /**
      * 브리드 복싱 이야기
      */
-    class breatheStory extends Board {
+    class breatheStory extends ImageBoard {
         
     }
 ?>
