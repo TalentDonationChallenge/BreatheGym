56b36eb2fc62ce247d78c2069d142a7073316074
Jonggyu Shim <cham42666@naver.com>
2015-10-03 15:18:27 +0900

    BreatheBoard 복붙

--- board/js/breatheStory.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $(".item").click(function () {
-		$("#zoom").addClass("show");
-	});
-	$(".close").click(function () {
-		$("#zoom").removeClass("show");
-	});
-});
