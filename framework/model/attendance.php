980b2b0e405ac2cc7074cc58fa45410e2bb9c715
Burningsweetpotato <87genesis@hanmail.net>
2015-10-06 06:55:41 +0900

    바코드 출석 관련 코드 추가
    -framework.php 에 attendance.php 참조하게 코드 추가
    
    - attendance.php 에 출석관련 db 클래스 작성
    
    - member/attendance/memberAttendance.php 에 바코드를 받아서 서버에 저장하는 코드 작

--- framework/model/attendance.php
+++ framework/model/attendance.php
@@ -3,8 +3,14 @@
      *
      */
     class Attendance {
-        function addAttendance($barcode) {
-            $pdo = Database::getInstace();
+        function addAttendance($barcode,$attendDate) {
+            
+			$pdo = Database::getInstance();
+			$stmt = $pdo->prepare("INSERT INTO attendance (barcode, date) VALUES (:barcode, :attendDate)");
+			$stmt -> execute(array(
+				':barcode'=> $barcode,
+				':attendDate' => $attendDate
+			));
             
         }
     }
