c6789b399ea8c5625b72a395d8748ecfda0bc6e1
lovelyhwado <lovelyhwado@gmail.com>
2015-10-08 19:26:06 +0900

    업로드 기능 만들었음. 추후 문제요소 살펴보기

--- community/freeboard/write.php
+++ community/freeboard/write.php
@@ -51,7 +51,7 @@ require_once(__DIR__.'/../../framework/framework.php');
 								<span>Add files...</span>
 								<input type="file" name="files[]" multiple>
 							</span>
-							<button type="submit" class="btn btn-primary start">
+							<!-- <button type="submit" class="btn btn-primary start">
 								<i class="fa fa-upload"></i>
 								<span>Start upload</span>
 							</button>
@@ -63,9 +63,9 @@ require_once(__DIR__.'/../../framework/framework.php');
 								<i class="fa fa-trash"></i>
 								<span>Delete</span>
 							</button>
-							<input type="checkbox" class="toggle">
+							<input type="checkbox" class="toggle"> -->
 							<!-- The global file processing state -->
-							<span class="fileupload-process"></span>
+							<!-- <span class="fileupload-process"></span> -->
 						</div>
 						<!-- The global progress state -->
 						<div class="col-lg-5 fileupload-progress fade">
@@ -90,86 +90,11 @@ require_once(__DIR__.'/../../framework/framework.php');
 			</div>
 		</section>
 	</section>
-	<script id="template-upload" type="text/x-tmpl">
-	{% for (var i=0, file; file=o.files[i]; i++) { %}
-	    <tr class="template-upload fade">
-	        <td>
-	            <span class="preview"></span>
-	        </td>
-	        <td>
-	            <p class="name">{%=file.name%}</p>
-	            <strong class="error text-danger"></strong>
-	        </td>
-	        <td>
-	            <p class="size">Processing...</p>
-	            <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
-	        </td>
-	        <td>
-	            {% if (!i && !o.options.autoUpload) { %}
-	                <button class="btn btn-primary start" disabled>
-	                    <i class="fa fa-upload"></i>
-	                    <span>Start</span>
-	                </button>
-	            {% } %}
-	            {% if (!i) { %}
-	                <button class="btn btn-warning cancel">
-	                    <i class="fa fa-ban"></i>
-	                    <span>Cancel</span>
-	                </button>
-	            {% } %}
-	        </td>
-	    </tr>
-	{% } %}
-	</script>
-	<!-- The template to display files available for download -->
-	<script id="template-download" type="text/x-tmpl">
-	{% for (var i=0, file; file=o.files[i]; i++) { %}
-	    <tr class="template-download fade">
-	        <td>
-	            <span class="preview">
-	                {% if (file.thumbnailUrl) { %}
-	                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
-	                {% } %}
-	            </span>
-	        </td>
-	        <td>
-	            <p class="name">
-	                {% if (file.url) { %}
-	                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}</a>
-	                {% } else { %}
-	                    <span>{%=file.name%}</span>
-	                {% } %}
-	            </p>
-	            {% if (file.error) { %}
-	                <div><span class="label label-danger">Error</span> {%=file.error%}</div>
-	            {% } %}
-	        </td>
-	        <td>
-	            <span class="size">{%=o.formatFileSize(file.size)%}</span>
-	        </td>
-	        <td>
-	            {% if (file.deleteUrl) { %}
-	                <button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
-	                    <i class="fa fa-trash"></i>
-	                    <span>Delete</span>
-	                </button>
-	                <input type="checkbox" name="delete" value="1" class="toggle">
-	            {% } else { %}
-	                <button class="btn btn-warning cancel">
-	                    <i class="fa fa-ban"></i>
-	                    <span>Cancel</span>
-	                </button>
-	            {% } %}
-	        </td>
-	    </tr>
-	{% } %}
-	</script>
 
 	<script src="/common/js/jquery-1.11.1.min.js"></script>
 	<script src="/common/js/bootstrap.min.js"></script>
 	<script src="/common/js/jquery.dcjqaccordion.2.7.js"></script>
 	<script src="/common/js/common-scripts.js"></script>
-	<script src="//blueimp.github.io/JavaScript-Templates/js/tmpl.min.js"></script>
 	<script src="//blueimp.github.io/JavaScript-Load-Image/js/load-image.all.min.js"></script>
 	<script src="//blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
 	<script src="//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
